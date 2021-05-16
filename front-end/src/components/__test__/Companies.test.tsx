c9b5c4ae826c7ffdd45b9173d4929fe7f5e98a0e
ebh <alex.dess@gmail.com>
2021-05-16 15:36:20 +1000

    More coverage for Companies component

--- front-end/src/components/__test__/Companies.test.tsx
+++ front-end/src/components/__test__/Companies.test.tsx
@@ -1,22 +1,36 @@
-import {shallow, ShallowWrapper} from "enzyme";
-import React from 'react';
 import Companies from "components/Companies";
-import CompaniesSearch from "components/CompaniesSearch";
 import CompaniesList from "components/CompaniesList";
+import CompaniesSearch from "components/CompaniesSearch";
+import {shallow, ShallowWrapper} from "enzyme";
+import React from "react";
 
 
-describe("Companies", () => {
+describe("Companies when first loaded", () => {
     let component: ShallowWrapper;
 
     beforeEach(() => {
-        component = shallow(<Companies />);
-    })
+        component = shallow(<Companies/>);
+    });
 
-    it('shows a CompaniesSearch component', () => {
+    it("shows a CompaniesSearch component", () => {
         expect(component.find(CompaniesSearch).length).toEqual(1);
-    })
+    });
 
-    it('shows a CompaniesList component', () => {
+    it("shows a CompaniesList component", () => {
         expect(component.find(CompaniesList).length).toEqual(1);
-    })
-})
+    });
+
+    it("has no companies when first loaded", () => {
+        const abc = component.find(CompaniesList).prop("companies");
+
+        expect(abc.length).toEqual(0);
+    });
+
+    it("has no exchanges when first loaded", () => {
+        expect(component.find(CompaniesSearch).prop("exchanges").length).toEqual(0);
+    });
+
+    it("has no filtered exchanges when first loaded", () => {
+        expect(component.find(CompaniesSearch).prop("exchangeNames").length).toEqual(0);
+    });
+});
